8e3fadabcb1a73f926cca0f99c86dcc7da1d10f8
Your Name <roihansori12@gmail.com>
2024-04-17 00:12:53 +0700

    first commit

---
+++ src/components/Billing.jsx
@@ -0,0 +1,44 @@
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../styles";
+
+const Billing = () => {
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img
+          src={bill}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+        <div className="absolute z-[3] -left-1/2 white__gradient w-[50%] h-[50%] top-0 rounded-full" />
+        <div className="absolute z-[0] -left-1/2 pink__gradient w-[50%] h-[50%] bottom-0" />
+      </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="sm:block hidden" /> billing &
+          invoicing.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px]`}>
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img
+            src={apple}
+            alt="apple_store"
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          />
+          <img
+            src={google}
+            alt="google_play_store"
+            className="w-[128px] h-[42px] object-contain cursor-pointer"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Billing;
